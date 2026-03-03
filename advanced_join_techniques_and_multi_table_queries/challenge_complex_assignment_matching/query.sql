7b2a805667728805ecc19c3f34fb9e6824e3e6d0
MilouSapinsky <>
2026-03-03 20:49:56 +0000

    Challenge: Complex Assignment Matching

---
+++ advanced_join_techniques_and_multi_table_queries/challenge_complex_assignment_matching/query.sql
@@ -0,0 +1,14 @@
+-- Write your code here
+SELECT 
+    a.assignment_id,
+    e.name AS employee_name,
+    p.project_name,
+    d.department_name
+FROM 
+    assignments a
+JOIN employees e
+    ON a.employee_id = e.employee_id
+JOIN projects p
+    ON a.project_id = p.project_id
+JOIN departments d
+    ON p.department_id = d.department_id;
